1ad52283fe420ec5ab86641d74caf096a3b45c40
Gabrick75 <gabriel.dijair.souza@gmail.com>
2026-01-30 14:25:58 -0300

    Project2 - Method readValidAge

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 import poo.Age;
 
+import static poo.Age.readValidAge;
+
 public class Main {
     public static void main(String[] args) {
-        byte age;
 
-        System.out.println("Qual a sua idade?");
         Scanner getAge = new Scanner(System.in);
-        age = getAge.nextByte();
+        byte age = readValidAge(getAge);
 
-        if (age < 0) {
-            System.out.println("Idade inválida! Não pode ser negativa.");
-            return; // encerra o programa
-        }
         Age.checkAge(age);
     }
 }
--- Project2/src/poo/Age.java
+++ Project2/src/poo/Age.java
@@ -1,6 +1,27 @@
 package poo;
 
+import java.util.Scanner;
+
 public class Age {
+
+    public static byte readValidAge(Scanner sc) {
+        int age;
+        boolean invalid;
+
+        do {
+            System.out.print("Digite a sua idade: ");
+            age = sc.nextInt();
+
+            invalid = age < 0 || age > 127;
+
+            if (invalid) {
+                System.out.println("Idade inválida! Digite um valor real.");
+            }
+
+        } while (invalid);
+
+        return (byte) age;
+    }
 
     public static void checkAge(byte age) {
         if (age < 0) {
